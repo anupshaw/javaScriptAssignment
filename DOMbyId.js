b58333055fac2fbbbeb416ceb0869b357086eb82
anupshaw <shawanup871@gmail.com>
2022-08-11 20:10:46 +0530

    first commit

---
+++ DOMbyId.js
@@ -0,0 +1,21 @@
+// console.dir(document);
+// console.log(document.domain)
+// console.log(document.title)
+// document.title=123;
+// console.log(document.title)
+// console.log(document.all[10])
+// console.log(document.all)
+// document.all[12].textContent="Hello"
+// console.log(document.getElementById("header"))
+var h=document.getElementById("header");
+// console.log(h);
+h.style.borderBottom="2px black solid"
+// h.textContent.style.borderBottom="2px black solid"    Doubt:how to add border under text
+// h.textContent="Hello"
+var h1=document.getElementById("title1");
+// h1.innerText="Heloo"
+h1.style.color="green";
+var h2=document.getElementById("title2");
+// h2.innerHTML='<h6>Heloo</h6>';  
+var ih=document.getElementById("header-title");
+ih.style.borderBottom="1px black solid" 
